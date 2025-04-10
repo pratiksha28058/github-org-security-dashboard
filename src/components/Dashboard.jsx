e0b2b2a055b960cdeaf888c5f1efadda4c6ec4d7
pratikshatiwari <47111738+pratikshatiwari@users.noreply.github.com>
2025-04-10 17:22:28 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { fetchRepositories, fetchSecurityAlerts } from './src/GithubService.jsx';
+import { fetchRepositories, fetchSecurityAlerts } from 'src/GithubService.jsx';
 import {
   LineChart,
   BarChart,
